b2ebb8ea1def4927a747e7a185174892506540ab
GainsGoblin <>
2022-12-08 00:24:25 +0100

    Improve readme and remove two unused interfaces

--- contracts/interfaces/INativeStableVault.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface INativeStableVault {
-    function depositNative() external payable;
-    function withdrawNative(uint256 _amount) external returns (uint256);
-}
